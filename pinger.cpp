284f6c109c34cf4f9a65cba70c5dd5af400d7466
Pieter Vandercammen <pieter.vandercammen@excentis.com>
2020-06-06 14:04:19 +0200

    Add architecture comments, cleanup destructor a bit

--- pinger.cpp
+++ pinger.cpp
@@ -1,5 +1,19 @@
 /**  Pings the whole IPv4 address space.
   *  This program is a work in progress.
+  *
+  * Basic architecture:
+  *   -> Pings are generated from 1 thread.
+  *       This thread adds a unique identifier to 
+  *       to each ping
+  *   -> A seperate thread inspects all IMCP traffic.
+  *      This thread is also responsible for making
+  *      the match between seed & receive.
+  * 
+  *  This approach has the advantage that we don't need
+  *  to cross thread boundaries. The main disadvantage is
+  *  that we place all the work in a single thread..
+  *   We assume that the network layer is the bottleneck.
+  *   
   */
 #include <iostream>
 #include <functional>
@@ -24,9 +38,20 @@ class ICMPCatcher
 
     Sniffer sniffer;
     std::thread thread_;
+
+    /** Sends a dummy ping.
+      * Unfortunately useful to stop ICMPCatcher.
+      * Libpcap+libTins are blocking on new packets.
+      */
+    void sendDummyPing(){
+        PacketSender sender;
+        IP pkt = IP("192.168.0.1") / ICMP(ICMP::ECHO_REQUEST) / RawPDU("foo");
+        sender.send(pkt);
+
+    }
 public:
-    ICMPCatcher():
-        sniffer("wlp2s0", MAX_PACKET_SIZE, PROMISCUOUS)
+    ICMPCatcher(const std::string& device):
+        sniffer(device, MAX_PACKET_SIZE, PROMISCUOUS, "icmp")
     {
         this->sniffer.set_timeout(1);
         thread_ = std::thread( [&]() {
@@ -35,8 +60,11 @@ public:
         });
     }
 
+    ICMPCatcher(const ICMPCatcher& oth) = delete;
+
     ~ICMPCatcher() {
         sniffer.stop_sniff();
+        sendDummyPing();
         thread_.join();
     }
 
@@ -53,7 +81,7 @@ public:
 
 int main(int argc, char** argv)
 {
-    ICMPCatcher cc;
+    ICMPCatcher pingReplies("wlp2s0");
 
     PacketSender sender;
     IP pkt = IP("192.168.0.1") / ICMP(ICMP::ECHO_REQUEST) / RawPDU("foo");
