ea764d30259e14f5e9edc77e4ad4d8849a9bb1c2
Pieter Vandercammen <pieter.vandercammen@excentis.com>
2020-06-15 21:38:29 +0200

    add rotate step for a bit more randomness

--- ipv4_util.h
+++ ipv4_util.h
@@ -2,12 +2,21 @@
 #define IPV4_UTIL_H
 
 #include <tins/tins.h>
+#include <iostream>
+#include <limits>
 
+using Tins::IPv4Address;
+using std::cout;
+using std::endl;
 
 IPv4Address random(IPv4Address addr){
-    uint32_t base_addr(addr);
-    uint64_t prime_val = 4294967311;
-    uint64_t other_prime = 100003;
-    return IPv4Address(uint64_t(base_addr) * prime_val + other_prime);
+    uint32_t result(addr);
+    uint64_t prime_val = 13340323177LLU;
+    for(size_t ctr(0); ctr < 3; ++ctr){
+	    result = uint64_t(result) * prime_val + 13;
+	    result = ((result << 11) | (result >> (32 -11)));
+    }
+
+    return IPv4Address(uint32_t(result));
 }
 #endif 
